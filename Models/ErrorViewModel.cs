e14b15bf8a33f28f7e4545a897eb1bd6e85b8788
CECILIA ABREU <49433586@ort>
2025-08-04 08:41:43 -0300

    Proyecto Creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP07_Wolman_Abreu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
